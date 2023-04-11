6d50119cb4040b02393aa7fd56f3afa738936bb0
Pavel <pavel.lagsan@educa.jcyl.es>
2023-04-11 11:57:01 +0200

    Segundo commit

--- src/Proyecto1/Principal1.java
+++ src/Proyecto1/Principal1.java
@@ -5,7 +5,7 @@ public class Principal1 {
 	public static void main(String[] args) {
 		System.out.println(" hola git ");
 		System.out.println(" Primer commit realizado ");
-		
+		System.out.println(" Segundo commit realizado ");
 
 
 	}
